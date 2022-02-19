b3bc6e4c9b3874782992a670c77e4e15e7d6b539
eugene <uwm1004@gmail.com>
2022-02-19 15:37:37 +0900

    client setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios'
 
 function App() {
+  const baseURL = 'http://localhost:3000/article/61f81fda54a285b7a9ab847a';
+  fetch(baseURL)
+    .then(resp => resp.json())
+    .then(data => displayData(data));
+  
+  function displayData(data) {
+    document.querySelector("#pre").innerHTML = JSON.stringify(data, null, 2);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div id="pre"></div>
     </div>
   );
 }
